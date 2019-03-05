f9b0f58261a60d97117c7d03403d7766b960086a
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-03-05 09:22:32 +0300

    Update function.h

--- RAND_CIPHER/function.h
+++ RAND_CIPHER/function.h
@@ -1,8 +1,11 @@
 #ifndef _function_h
 #define _function_h
 
+#include <stdint.h>
+
 void swap (unsigned char *a, unsigned char  *b);
 int GCD (int a, int b);
+void crack_text (unsigned char *text, uint32_t *Left, uint32_t *Right, int len_text);
 char *Salt();
 
 #endif
