ce5cfbed167c88e08d8b9a8366fa4bad0f90650c
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-03-05 09:18:42 +0300

    Update Blowfish.h

--- RAND_CIPHER/Blowfish.h
+++ RAND_CIPHER/Blowfish.h
@@ -5,7 +5,6 @@
 
 void Blowfish_Init (unsigned char *key, int keybytes);
 void Blowfish_Encipher (uint32_t *L, uint32_t *R);
-void crack_text (unsigned char *text, uint32_t *Left, uint32_t *Right, int text_len);
 unsigned char *Blowfish_key();
 uint32_t F (uint32_t x);
 
